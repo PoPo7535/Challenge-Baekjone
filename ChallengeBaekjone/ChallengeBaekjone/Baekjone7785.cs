2085f4cbca6239500633e2287d3b275507f23b49
PoPo <dltngus7535@gmail.com>
2024-06-23 03:11:06 +0900

    백준 7785번 문제

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone7785.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone7785
+    {
+        public static void Problem()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var hashSet = new HashSet<string>();
+            for (int i = 0; i < N; ++i)
+            {
+                var str = Console.ReadLine().Split(' ');
+                if (str[1] == "enter")
+                    hashSet.Add(str[0]);
+                else
+                    hashSet.Remove(str[0]);
+            }
+            var list = new List<string>(hashSet);
+            list.Sort();
+            list.Reverse();
+            using (StreamWriter writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
+            {
+                for (int i = 0; i < list.Count; ++i)
+                {
+                    writer.WriteLine($"{list[i]}");
+                }
+            }
+        }
+    }
+}
