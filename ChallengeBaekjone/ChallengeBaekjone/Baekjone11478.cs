10029a4d3e535dedf9e2fd959beddbf79e28a764
PoPo <dltngus7535@gmail.com>
2024-06-23 03:53:11 +0900

    백준 11478번 문제

--- ChallengeBaekjone/ChallengeBaekjone/Baekjone11478.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone11478.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 
 namespace ChallengeBaekjone
 {
-    internal class Program
+    public class Baekjone11478
     {
-        public static void Main(string[] args)
+        public static void Problem()
         {
             var str = Console.ReadLine();
             var hash = new HashSet<string>();
-
             for (int i = 1; i <= str.Length; ++i)
             {
                 for (int j = 0; j < str.Length - i+1; ++j)
@@ -21,18 +20,3 @@ namespace ChallengeBaekjone
         }
     }
 }
-
-/*
- * var input = Console.ReadLine().Split(' ');
- * var N = int.Parse(input[0]);
- * var M = int.Parse(input[1]);
- * 
- * using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
- * {
- *     for (int i = 0; i < N; ++i)
- *     {
- *         var key = Console.ReadLine();
- *         writer.WriteLine(dic[key]);
- *     }
- * }
- */
