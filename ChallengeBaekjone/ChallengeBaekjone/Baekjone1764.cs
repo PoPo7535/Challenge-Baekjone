a5115972a2a69bba540f26a83dc51824d3af71c1
PoPo <dltngus7535@gmail.com>
2024-06-23 03:31:15 +0900

    백준 1764번 문제

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone1764.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ChallengeBaekjone.Properties
+{
+    public class Baekjone1764
+    {
+        public static void Problem()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var N = int.Parse(input[0]);
+            var M = int.Parse(input[1]);
+            var hashSet1 = new HashSet<string>();
+            var hashSet2 = new HashSet<string>();
+            for (int i = 0; i < N; ++i)
+                hashSet1.Add(Console.ReadLine());
+            for (int i = 0; i < M; ++i)
+                hashSet2.Add(Console.ReadLine());
+            using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
+            {
+                var list = hashSet1.Where(str => hashSet2.Contains(str)).ToList();
+    
+                list.Sort();
+                writer.WriteLine(list.Count);
+                foreach (var str in list)
+                    writer.WriteLine(str);
+            }
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ChallengeBaekjone
@@ -9,25 +10,9 @@ namespace ChallengeBaekjone
     {
         public static void Main(string[] args)
         {
-            var input = Console.ReadLine().Split(' ');
-            var N = int.Parse(input[0]);
-            var M = int.Parse(input[1]);
-            var dic = new Dictionary<string, string>();
 
-            for (int i = 1; i <= N; ++i)
-            {
-                var name = Console.ReadLine();
-                dic.Add(name, i.ToString());
-                dic.Add(i.ToString(), name);
-            }
-            using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
-            {
-                for (int i = 1; i <= M; ++i)
-                {
-                    var key = Console.ReadLine();
-                    writer.WriteLine(dic[key]);
-                }
-            }
+            
+
         }
     }
 }
