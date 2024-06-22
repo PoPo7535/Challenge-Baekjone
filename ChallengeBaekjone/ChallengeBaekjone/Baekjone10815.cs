c385c13096343bfa7fe680e07456a80ec29ff7f4
PoPo <dltngus7535@gmail.com>
2024-06-22 09:19:32 +0900

    백준 10815번 문제

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone10815.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone10815
+    {
+        public static void Problem()
+        {
+            var aCount = int.Parse(Console.ReadLine());
+            var split = Console.ReadLine().Split(' ');
+            
+            var aNumbers = new HashSet<int>();
+            for (int i = 0; i < aCount; ++i)
+                aNumbers.Add(int.Parse(split[i]));
+            
+            var bCount = int.Parse(Console.ReadLine());
+            split = Console.ReadLine().Split(' ');
+            var bNumbers = new int[bCount];
+            
+            for (int i = 0; i < bCount; ++i)
+                bNumbers[i] = int.Parse(split[i]);
+            
+            using (StreamWriter writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
+            {
+                for (int i = 0; i < bNumbers.Length; ++i)
+                {
+                    var result = aNumbers.Contains(bNumbers[i]) ? 1 : 0;
+                    writer.Write($"{result} ");
+                }
+            }
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace ChallengeBaekjone
@@ -10,25 +9,7 @@ namespace ChallengeBaekjone
     {
         public static void Main(string[] args)
         {
-            var N = int.Parse(Console.ReadLine());
-            var dic = new Dictionary<int, int>();
-            var arr = new int[N];
-            var split = Console.ReadLine().Split(' ');
-            for (int i = 0; i < N; ++i)
-                arr[i] = int.Parse(split[i]);
-
-            var dis = arr.Distinct().ToArray();
-            Array.Sort(dis);
-            for (int i = 0; i < dis.Length; ++i)
-                dic.Add(dis[i], i);
-
-            using (StreamWriter writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
-            {
-                for (int i = 0; i < arr.Length; ++i)
-                {
-                    writer.Write($"{dic[arr[i]]} ");
-                }
-            }
+            
         }
     }
 }
