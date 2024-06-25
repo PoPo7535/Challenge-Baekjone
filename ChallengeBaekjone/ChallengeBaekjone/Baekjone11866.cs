daaad1439c77ba3c471a2c5f4e3014180e015246
PoPo <dltngus7535@gmail.com>
2024-06-26 06:40:18 +0900

    백준 11866번 문제 - 요세푸스 문제 0

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone11866.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone11866
+    {
+        public static void Problem()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            var input = Console.ReadLine().Split(' ');
+            var N = int.Parse(input[0]);
+            var M = int.Parse(input[1]);
+            for (int i = 1; i <= N; ++i)
+                list.AddLast(i);
+            Console.Write('<');
+            var node = list.First;
+            while (0 != list.Count)
+            {
+                for (int i = 0; i < M-1; ++i)
+                {
+                    node = node.Next ?? list.First;
+                }
+                var bumpNode = node.Next ?? list.First;
+                Console.Write(node.Value);
+                list.Remove(node);
+                if(list.Count != 0)
+                    Console.Write(", ");
+                node= bumpNode;
+            }
+            Console.Write('>');
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -7,6 +7,7 @@ namespace ChallengeBaekjone
     {
         public static void Main(string[] args)
         {
+
         }
     }
 }
