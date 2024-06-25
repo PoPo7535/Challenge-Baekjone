b28e7e3768c21733f2db48086df0a330a1068fff
PoPo <dltngus7535@gmail.com>
2024-06-26 07:04:40 +0900

    백준 15439번 문제 - 베라의 패션

---
+++ ChallengeBaekjone/ChallengeBaekjone/BaekJone15439.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class BaekJone15439
+    {
+        public static void Problem()
+        {
+            var N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N * (N - 1));
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -9,49 +9,10 @@ namespace ChallengeBaekjone
     {
         public static void Main(string[] args)
         {
-            var deck = new LinkedList<int>();
             var N = int.Parse(Console.ReadLine());
-            
-            using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
-            {
-                for (int i = 0; i < N; ++i)
-                {
-                    var input = Console.ReadLine().Split(' ');
-                    var commend = int.Parse(input[0]);
-                    switch (commend)
-                    {
-                        case 1:
-                            deck.AddFirst(int.Parse(input[1]));
-                            break;
-                        case 2:
-                            deck.AddLast(int.Parse(input[1]));
-                            break;
-                        case 3:
-                            writer.WriteLine(deck.Count == 0 ? -1 : deck.First.Value);
-                            if (deck.Count != 0)
-                                deck.RemoveFirst();
-                            break;
-                        case 4:
-                            writer.WriteLine(deck.Count == 0 ? -1 : deck.Last.Value);
-                            if (deck.Count != 0)
-                                deck.RemoveLast();
-                            break;
-                        case 5:
-                            writer.WriteLine(deck.Count);
-                            break;
-                        case 6:
-                            writer.WriteLine(deck.Count == 0 ? 1 : 0);
-                            break;
-                        case 7:
-                            writer.WriteLine(deck.Count == 0 ? -1 : deck.First.Value);
-                            break;
-                        case 8:
-                            writer.WriteLine(deck.Count == 0 ? -1 : deck.Last.Value);
-                            break;
-                    }
-                }
-            }
-           
+            Console.WriteLine(N * (N - 1));
+
+
         }
     }
 }
