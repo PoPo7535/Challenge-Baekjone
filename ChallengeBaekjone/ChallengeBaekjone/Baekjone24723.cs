e82e1f6978b9612b2a03bc2900e3da0f2266f6d5
PoPo <dltngus7535@gmail.com>
2024-06-28 05:24:08 +0900

    백준 24723번 문제 - 녹색거탑

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone24723.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone24723
+    {
+        public static void Probelm()
+        {
+            Console.WriteLine(Math.Pow(2,int.Parse(Console.ReadLine())));
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,41 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-
 namespace ChallengeBaekjone
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
-            var N = int.Parse(sr.ReadLine());
-            var A = sr.ReadLine().Split(' ');
-            var B = sr.ReadLine().Split(' ');
-            var list = new LinkedList<int>();
-            for (var i = 0; i < N; ++i)
-            {
-                if (A[i][0] == '0')
-                    list.AddLast(int.Parse(B[i]));
-            }
-
-            var M = int.Parse(sr.ReadLine());
-            var C = sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            
-            using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
-            {
-                list.AddLast(int.MaxValue);
-                var lastNode = list.Last;
-                for (int i = 0; i < M; ++i)
-                {
-                    list.AddFirst(C[i]);
-                    lastNode = lastNode.Previous;
-                    writer.Write($"{lastNode.Value} ");
-                }
-            }
-            sr.Close();
+            Console.WriteLine(Math.Pow(2,int.Parse(Console.ReadLine())));
         }
     }
 }
