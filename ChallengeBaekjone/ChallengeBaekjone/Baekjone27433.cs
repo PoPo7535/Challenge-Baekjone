0a3740cb86fb7e8c12958fc918611ca83e25d604
PoPo <dltngus7535@gmail.com>
2024-06-29 06:44:06 +0900

    백준 27433번 문제 - 팩토리얼 2

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone27433.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone27433
+    {
+        public static void Problem()
+        {
+            Console.WriteLine(Factorial(int.Parse(Console.ReadLine())));   
+        }
+        static long Factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,49 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-
 namespace ChallengeBaekjone
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var inputs = Console.ReadLine().Split(' ');
-            var N = int.Parse(inputs[0]);
-            var M = int.Parse(inputs[1]);
-            var dic = new Dictionary<string, int>();
-            for (int i = 0; i < N; ++i)
-            {
-                var input = Console.ReadLine();
-                if(input.Length < M)
-                    continue;
-                if (dic.ContainsKey(input))
-                    ++dic[input];
-                else
-                    dic.Add(input, 1);
-            }
-            var list = dic.ToList();
-            list.Sort((a, b) =>
-            {
-                var com = b.Value.CompareTo(a.Value);
-                if (0 != com)
-                    return com;
-                com = b.Key.Length.CompareTo(a.Key.Length);
-                if (0 != com)
-                    return com;
-                return string.Compare(a.Key, b.Key, StringComparison.Ordinal);
-            });
-            
-            using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
-            {
-                for (int i = 0; i < list.Count; ++i)
-                {
-                    writer.WriteLine(list[i].Key);
-                }
-            }
+        
         }
+
     }
 }
 
