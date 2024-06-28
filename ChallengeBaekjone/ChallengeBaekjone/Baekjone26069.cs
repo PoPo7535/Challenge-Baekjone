83de3d90f4803e4412674905707053bb032c1870
PoPo <dltngus7535@gmail.com>
2024-06-29 04:57:54 +0900

    백준 26069번 문제 - 붙임성 좋은 총총이

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone26069.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone26069
+    {
+        public static void Problem()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var rainbow = new HashSet<string> { "ChongChong" };
+            for (int i = 0; i < N; ++i)
+            {
+                var rabbit = Console.ReadLine().Split(' ');
+                if (rainbow.Contains(rabbit[0]) || rainbow.Contains(rabbit[1]))
+                {
+                    rainbow.Add(rabbit[0]);
+                    rainbow.Add(rabbit[1]);
+                }
+            }
+            Console.WriteLine(rainbow.Count);
+        }
+    }
+}
