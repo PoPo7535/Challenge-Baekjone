21206da41d14d29c3a3f38041f0ae0c48b513a5a
PoPo <dltngus7535@gmail.com>
2024-06-30 03:05:49 +0900

    백준 4779번 문제 - 칸토어 집합

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone4779.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone4779
+    {
+        public void Problem()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    break;
+                var len =  (int)Math.Pow(3, int.Parse(input));
+                var sb = new StringBuilder(len);
+                for (int i = 0; i < len; ++i)
+                    sb.Append('-');
+                Cal(sb, 0, sb.Length);
+                Console.WriteLine(sb);
+            }
+        }
+        public static void Cal(StringBuilder sb, int startIndex, int len)
+        {
+            if (len <= 1)
+                return;
+            len /= 3;
+            for (int i = 0; i < len; ++i)
+                sb[startIndex + len + i] = ' ';
+
+            Cal(sb, startIndex, len);
+            Cal(sb, startIndex + (len * 2), len);
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,67 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace ChallengeBaekjone
 {
     internal class Program
     {
-        public static void MergeSortArray(int[] A, int p, int r)
-        {
-            if (p < r)
-            {
-                int q = (p + r) / 2;
-                MergeSortArray(A, p, q);       // 전반부 정렬
-                MergeSortArray(A, q + 1, r);   // 후반부 정렬
-                Merge(A, p, q, r);             // 병합
-            }
-        }
-
-
-        private static void Merge(int[] A, int p, int q, int r)
-        {
-            int i = p, j = q + 1, t = 0;
-            int[] tmp = new int[r - p + 1];
-
-            while (i <= q && j <= r)
-            {
-                if (A[i] <= A[j])
-                    tmp[t++] = A[i++];
-                else
-                    tmp[t++] = A[j++];
-            }
-
-            while (i <= q)
-            {
-                tmp[t++] = A[i++];
-            }
-
-            while (j <= r)
-            {
-                tmp[t++] = A[j++];
-            }
-
-            for (i = p, t = 0; i <= r; i++, t++)
-            {
-                ++count;
-                A[i] = tmp[t];
-                if (K == count)
-                    se = A[i];
-            }
-        }
-
-        private static int count = 0;
-        private static int K = 0;
-        private static int se = -1;
         public static void Main(string[] args)
         {
-            var str = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            K = str[1];
-            var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            MergeSortArray(arr, 0, arr.Length - 1);
-            Console.WriteLine(se);
-        }
 
+        }
     }
 }
 
