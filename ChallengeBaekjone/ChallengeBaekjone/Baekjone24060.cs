b8aa987bae6089544a4c6a3576c7e345caa46eb2
PoPo <dltngus7535@gmail.com>
2024-06-30 02:00:22 +0900

    백준 24060번 문제 - 알고리즘 수업 - 병합 정렬 1

--- ChallengeBaekjone/ChallengeBaekjone/Baekjone24060.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone24060.cs
@@ -1,59 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace ChallengeBaekjone
 {
-    internal class Program
+    public class Baekjone24060
     {
         public static void MergeSortArray(int[] A, int p, int r)
         {
-            if (p < r)
-            {
-                int q = (p + r) / 2;
-                MergeSortArray(A, p, q);       // 전반부 정렬
-                MergeSortArray(A, q + 1, r);   // 후반부 정렬
-                Merge(A, p, q, r);             // 병합
-            }
+            if (p >= r) 
+                return;
+            int q = (p + r) / 2;
+            MergeSortArray(A, p, q);       // 전반부 정렬
+            MergeSortArray(A, q + 1, r);   // 후반부 정렬
+            Merge(A, p, q, r);             // 병합
         }
-
-
+        
         private static void Merge(int[] A, int p, int q, int r)
         {
             int i = p, j = q + 1, t = 0;
             int[] tmp = new int[r - p + 1];
-
+        
             while (i <= q && j <= r)
-            {
                 if (A[i] <= A[j])
                     tmp[t++] = A[i++];
                 else
                     tmp[t++] = A[j++];
-            }
-
+        
             while (i <= q)
-            {
                 tmp[t++] = A[i++];
-            }
-
+        
             while (j <= r)
-            {
                 tmp[t++] = A[j++];
-            }
-
+        
             for (i = p, t = 0; i <= r; i++, t++)
-            {
                 ++count;
                 A[i] = tmp[t];
                 if (K == count)
                     se = A[i];
-            }
         }
-
+        
         private static int count = 0;
         private static int K = 0;
         private static int se = -1;
-        public static void Main(string[] args)
+        public static void Problem()
         {
             var str = Console.ReadLine().Split().Select(int.Parse).ToArray();
             K = str[1];
@@ -61,22 +50,5 @@ namespace ChallengeBaekjone
             MergeSortArray(arr, 0, arr.Length - 1);
             Console.WriteLine(se);
         }
-
     }
 }
-
-
-/*
- * var input = Console.ReadLine().Split(' ');
- * var N = int.Parse(input[0]);
- * var M = int.Parse(input[1]);
- * 
- * using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
- * {
- *     for (int i = 0; i < N; ++i)
- *     {
- *         var key = Console.ReadLine();
- *         writer.WriteLine(dic[key]);
- *     }
- * }
- */
