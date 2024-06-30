da9390312bf230002a293c6ce1054d6e79a3911c
PoPo <dltngus7535@gmail.com>
2024-07-01 06:39:17 +0900

    백준 15649번 문제 - N과 M (1)

--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,21 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 namespace ChallengeBaekjone
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var input = Console.ReadLine().Split();
-            var a1 = int.Parse(input[0]);
-            var a0 = int.Parse(input[1]);
-        
-            var c = int.Parse(Console.ReadLine());
-            var n0 = int.Parse(Console.ReadLine());
-        
-            if (a1 <= c && a0 <= (c - a1) * n0)
-                Console.WriteLine(1);
-            else
-                Console.WriteLine(0);
+            
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+
+            List<int> sequence = new List<int>();
+            bool[] visited = new bool[N + 1];
+            using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
+            {
+                NM(N, M, sequence, visited, writer);
+            }
+        }
+
+        static void NM(int N, int M, List<int> sequence, bool[] visited, StreamWriter writer)
+        {
+            if (sequence.Count == M)
+            {
+                writer.WriteLine(string.Join(" ", sequence));
+                return;
+            }
+
+            for (int i = 1; i <= N; ++i)
+            {
+                if (!visited[i])
+                {
+                    visited[i] = true;
+                    sequence.Add(i);
+                    NM(N, M, sequence, visited, writer);
+                    sequence.RemoveAt(sequence.Count - 1);
+                    visited[i] = false;
+                }
+            }
         }
     }
 }
