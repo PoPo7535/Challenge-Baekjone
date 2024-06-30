68983710fa49bffbb3109492f5232ea449d01bd8
PoPo <dltngus7535@gmail.com>
2024-07-01 06:45:09 +0900

    백준 15650번 문제 - N과 M (2)

--- ChallengeBaekjone/ChallengeBaekjone/Baekjone15650.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone15650.cs
@@ -5,36 +5,34 @@ using System.Text;
 
 namespace ChallengeBaekjone
 {
-    public class Baekjone15649
+    public class Baekjone15650
     {
         public static void Problem()
         {
             string[] input = Console.ReadLine().Split();
             int N = int.Parse(input[0]);
             int M = int.Parse(input[1]);
-
             List<int> sequence = new List<int>();
             bool[] visited = new bool[N + 1];
             using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
             {
-                NM(N, M, sequence, visited, writer);
+                NM(1, N, M, sequence, visited, writer);
             }
         }
-        static void NM(int N, int M, List<int> sequence, bool[] visited, StreamWriter writer)
+        static void NM(int I, int N, int M, List<int> sequence, bool[] visited, StreamWriter writer)
         {
             if (sequence.Count == M)
             {
                 writer.WriteLine(string.Join(" ", sequence));
                 return;
             }
-
-            for (int i = 1; i <= N; ++i)
+            for (int i = I; i <= N; ++i)
             {
                 if (!visited[i])
                 {
                     visited[i] = true;
                     sequence.Add(i);
-                    NM(N, M, sequence, visited, writer);
+                    NM(i, N, M, sequence, visited, writer);
                     sequence.RemoveAt(sequence.Count - 1);
                     visited[i] = false;
                 }
