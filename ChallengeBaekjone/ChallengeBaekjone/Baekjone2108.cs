e376272cf5b4eb5c7185fc549e67ffab2a22cdd0
PoPo <dltngus7535@gmail.com>
2024-07-01 02:26:32 +0900

    백준 2180번 문제 - 통계학

--- ChallengeBaekjone/ChallengeBaekjone/Baekjone2108.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone2108.cs
@@ -1,10 +1,68 @@
-﻿namespace ChallengeBaekjone
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengeBaekjone
 {
     public class Baekjone2108
     {
         public static void Problem()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+            var dic = new Dictionary<int, int>();
+            var average = 0D;
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            for (int i = 0; i < N; ++i)
+            {
+                var input = int.Parse(Console.ReadLine());
+                average += input;
+                if (input < min)
+                    min = input;
+                if (max < input)
+                    max = input;
+                if (dic.ContainsKey(input))
+                    ++dic[input];
+                else
+                    dic.Add(input, 1);
+    
+                list.Add(input);
+            }
+            list.Sort();
+            var dicToList = dic.ToList();
+            dicToList.Sort((a, b) =>
+            {
+                var compareTo= a.Value.CompareTo(b.Value);
+                if (compareTo == 0)
+                    compareTo = b.Key.CompareTo(a.Key);
+                return compareTo;
+            });
+            var val =  1<dicToList.Count &&
+                       (dicToList[dicToList.Count - 1].Value == dicToList[dicToList.Count - 2].Value)
+                ? dicToList[dicToList.Count - 2].Key
+                : dicToList[dicToList.Count - 1].Key;
+            Console.Write($"" +
+                          $"{RoundToTenths(average / N):0}\n" +
+                          $"{list[list.Count / 2]}\n" +
+                          $"{val}\n" +
+                          $"{max - min}");
+        }
+
+
+        static int RoundToTenths(double value)
+        {
+            double scaledValue = value;
+        
+            // 양수의 경우
+            if (scaledValue > 0)
+                scaledValue = (int)(scaledValue + 0.5);
+            // 음수의 경우
+            else
+                scaledValue = (int)(scaledValue - 0.5);
+        
+            // 다시 10으로 나누어 소수점 첫째 자리까지 반올림된 값으로 변환
+            return (int)scaledValue;
         }
     }
 }
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -10,21 +10,9 @@ namespace ChallengeBaekjone
     {
         public static void Main(string[] args)
         {
-            using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
-            {
-                var n = int.Parse(Console.ReadLine());
-                writer.WriteLine($"{(int)Math.Pow(2, n) - 1}");
-                Hanoi(n, 1, 2, 3, writer);
-            }
+            
+        }
 
-        }
-        static void Hanoi(int n, int from, int by, int to, StreamWriter writer)
-        {
-            if (n == 0) return;
-            Hanoi(n - 1, from, to, by, writer);
-            writer.WriteLine($"{from} {to}");
-            Hanoi(n - 1, by, from, to, writer);
-        }
     }
 }
 
