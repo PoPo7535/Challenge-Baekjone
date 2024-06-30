98e3f8bef08f82f8c7ba9b4a8f1820bf140eac61
PoPo <dltngus7535@gmail.com>
2024-07-01 02:28:48 +0900

    백준 24263번 문제 - 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone24263.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone24263
+    {
+        public static void Problem()
+        {
+            var i = int.Parse(Console.ReadLine());
+            Console.WriteLine(i);
+            Console.WriteLine(1);  
+        }
+    }
+}
