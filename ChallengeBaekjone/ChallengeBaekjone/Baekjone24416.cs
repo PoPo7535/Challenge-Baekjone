d2cd999d49ac618f06efb60c9a467a1493b181b1
PoPo <dltngus7535@gmail.com>
2024-07-02 05:41:07 +0900

    백준 24416번 문제 - 알고리즘 수업 - 피보나치 수 1

--- ChallengeBaekjone/ChallengeBaekjone/Baekjone24416.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone24416.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 
 namespace ChallengeBaekjone
 {
-    internal class Program
+    public class Baekjone24416
     {
-        static void Main(string[] args)
+        static void Problem()
         {
             var N = int.Parse(Console.ReadLine());
             Console.WriteLine($"{Fibonacci(N)} {N-2}");
@@ -18,31 +15,13 @@ namespace ChallengeBaekjone
             long a = 0;
             long b = 1;
             long c = 0;
-
             for (int i = 2; i <= n; i++)
             {
                 c = a + b;
                 a = b;
                 b = c;
             }
-
             return c;
         }
     }
 }
-
-
-/*
- * var input = Console.ReadLine().Split(' ');
- * var N = int.Parse(input[0]);
- * var M = int.Parse(input[1]);
- * 
- * using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
- * {
- *     for (int i = 0; i < N; ++i)
- *     {
- *         var key = Console.ReadLine();
- *         writer.WriteLine(dic[key]);
- *     }
- * }
- */
