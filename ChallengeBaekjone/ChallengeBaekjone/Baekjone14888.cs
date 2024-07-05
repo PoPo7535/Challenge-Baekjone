758cb3a1cfc86d656f0672e4e0f594cacf2e5142
PoPo <dltngus7535@gmail.com>
2024-07-06 04:32:58 +0900

    백준 14888번 문제 - 연산자 끼워넣기

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone14888.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone14888
+    {
+        static int N;
+        static int[] numbers;
+        static int[] operators; 
+        static int maxValue = int.MinValue;
+        static int minValue = int.MaxValue;
+        public static void Problem()
+        {
+            N = int.Parse(Console.ReadLine());
+            numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            operators = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Backtrack(numbers[0], 1);
+            Console.WriteLine(maxValue);
+            Console.WriteLine(minValue);
+        }
+        static void Backtrack(int curVal, int index)
+        {
+            if (index == N)
+            {
+                maxValue = Math.Max(maxValue, curVal);
+                minValue = Math.Min(minValue, curVal);
+                return;
+            }
+            for (int i = 0; i < 4; ++i)
+            {
+                if (operators[i] <= 0) 
+                    continue;
+                --operators[i];
+                if (i == 0)
+                    Backtrack(curVal + numbers[index], index + 1);
+                else if (i == 1)
+                    Backtrack(curVal - numbers[index], index + 1);
+                else if (i == 2)
+                    Backtrack(curVal * numbers[index], index + 1);
+                else if (i == 3)
+                    Backtrack(curVal / numbers[index], index + 1);
+                ++operators[i];
+            }
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Numerics;
-using System.Text;
+using System.Linq;
 
 namespace ChallengeBaekjone
 {
     internal class Program
     {
-
-
-
-
-     
-
+      
     }
 }
 
@@ -32,21 +24,3 @@ namespace ChallengeBaekjone
  *     }
  * }
  */
-
-
-// static int W(int a, int b, int c)
-// {
-//     if (a <= 0 || b <= 0 || c <= 0)
-//     {
-//         return 1;
-//     }
-//     if (a > 20 || b > 20 || c > 20)
-//     {
-//         return W(20, 20, 20);
-//     }
-//     if (a < b && b < c)
-//     {
-//         return W(a, b, c - 1) + W(a, b - 1, c - 1) - W(a, b - 1, c);
-//     }
-//     return W(a - 1, b, c) + W(a - 1, b - 1, c) + W(a - 1, b, c - 1) - W(a - 1, b - 1, c - 1);
-// }
