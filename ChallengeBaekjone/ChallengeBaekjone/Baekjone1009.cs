9fed689fd2a0dd0823158b889e063a2b499d77bf
PoPo <dltngus7535@gmail.com>
2024-07-12 23:06:28 +0900

    백준 1009번 문제 - 분산처리

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone1009.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone1009
+    {
+        public static void Problem()
+        {
+            var N = int.Parse(Console.ReadLine());
+            for (int t = 0; t < N; ++t)
+            {
+                var inputs = Console.ReadLine().Split();
+                var a = int.Parse(inputs[0]);
+                var b = int.Parse(inputs[1]);
+                // a의 마지막 자리수만 고려
+                a = a % 10;
+                if (a == 0) 
+                {
+                    Console.WriteLine(10);
+                    continue;
+                }
+                int result = 1;
+                for (int i = 0; i < b; i++)
+                    result = (result * a) % 10;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -7,19 +7,10 @@ namespace ChallengeBaekjone
     {
         static void Main()
         {
-            var N = int.Parse(Console.ReadLine());
-            var dp = new int[N + 1];
 
-            for (int i = 2; i <= N; i++)
-            {
-                dp[i] = dp[i - 1] + 1;
-                if (i % 2 == 0)
-                    dp[i] = Math.Min(dp[i], dp[i / 2] + 1);
-                if (i % 3 == 0)
-                    dp[i] = Math.Min(dp[i], dp[i / 3] + 1);
-            }
 
-            Console.WriteLine(dp[N]);
+
+
         }
     }
 }
