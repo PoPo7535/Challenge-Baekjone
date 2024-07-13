121c729b316809b9d720c36b5d2134965e933c62
PoPo <dltngus7535@gmail.com>
2024-07-13 23:41:39 +0900

    백준 1032번 문제 - 명령 프롬프트

--- ChallengeBaekjone/ChallengeBaekjone/Baekjone1032.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone1032.cs
@@ -3,19 +3,17 @@ using System.Text;
 
 namespace ChallengeBaekjone
 {
-    internal class Program
+    public class Baekjone1032
     {
-        static void Main()
+        public static void Problem()
         {
             var N = int.Parse(Console.ReadLine());
-
             if (N == 0)
                 return;
             var sb = new StringBuilder(Console.ReadLine());
             for (int i = 0; i < N - 1; ++i)
             {
                 var str = Console.ReadLine();
-                
                 for (int j = 0; j < sb.Length; ++j)
                 {
                     if (sb[j] != str[j])
@@ -26,19 +24,3 @@ namespace ChallengeBaekjone
         }
     }
 }
-
-
-/*
- * var input = Console.ReadLine().Split(' ');
- * var N = int.Parse(input[0]);
- * var M = int.Parse(input[1]);
- * 
- * using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
- * {
- *     for (int i = 0; i < N; ++i)
- *     {
- *         var key = Console.ReadLine();
- *         writer.WriteLine(dic[key]);
- *     }
- * }
- */
