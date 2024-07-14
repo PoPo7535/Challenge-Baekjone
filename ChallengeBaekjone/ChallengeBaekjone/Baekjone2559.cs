f9be92a9209cc3d22a474a770933c3264f6cf094
PoPo <dltngus7535@gmail.com>
2024-07-15 05:18:11 +0900

    백준 2559번 문제 - 수열

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone2559.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone2559
+    {
+        public static void Problem()
+        {
+            var input = Console.ReadLine().Split();
+            var N = int.Parse(input[0]);
+            var K = int.Parse(input[1]);
+            var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var maxVal = 0;
+            for (int i = 0; i < K; ++i)
+                maxVal += arr[i];
+            var dp = maxVal;
+            for (int i = K; i < N; ++i)
+            {
+                dp = dp + arr[i] - arr[i - K];
+                if (maxVal < dp)
+                    maxVal = dp;
+            }
+            Console.WriteLine(maxVal);
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace ChallengeBaekjone
 {
@@ -15,16 +12,8 @@ namespace ChallengeBaekjone
 
 
 /*
- * var input = Console.ReadLine().Split(' ');
- * var N = int.Parse(input[0]);
- * var M = int.Parse(input[1]);
- * 
- * using (var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default, bufferSize: 1024))
- * {
- *     for (int i = 0; i < N; ++i)
- *     {
- *         var key = Console.ReadLine();
- *         writer.WriteLine(dic[key]);
- *     }
- * }
+ * var sr = new StreamReader(Console.OpenStandardInput());
+ * var sw = new StreamWriter(Console.OpenStandardOutput());
+ * sr.Close();
+ * sw.Close();
  */
