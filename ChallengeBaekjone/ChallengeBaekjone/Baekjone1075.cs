67613e1bf1c984db668bf0c0bbe28ad0df506686
PoPo <dltngus7535@gmail.com>
2024-07-17 08:43:50 +0900

    백준 1075번 문제 - 나누기

--- ChallengeBaekjone/ChallengeBaekjone/Baekjone1075.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone1075.cs
@@ -3,28 +3,19 @@ using System.IO;
 
 namespace ChallengeBaekjone
 {
-    internal class Program
+    public class Baekjone1075
     {
-        static void Main()
+        public static void Problem()
         {
             var sr = new StreamReader(Console.OpenStandardInput());
             var sw = new StreamWriter(Console.OpenStandardOutput());
-
             var N = sr.ReadLine();
             var F = int.Parse(sr.ReadLine());
             var val = int.Parse(N.Substring(0, N.Length - 2)) * 100;
             var n = val % F;
-            sw.WriteLine((F - n).ToString().PadLeft(2,'0'));
+            sw.WriteLine(n == 0 ? "00" : F - n < 10 ? $"0{F - n}" : $"{F - n}");
             sr.Close();
             sw.Close();
         }
     }
 }
-
-
-/*
- * var sr = new StreamReader(Console.OpenStandardInput());
- * var sw = new StreamWriter(Console.OpenStandardOutput());
- * sr.Close();
- * sw.Close();
- */
