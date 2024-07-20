a2c0197689318df1a1708d3ff5397c0a1585fcf8
PoPo <dltngus7535@gmail.com>
2024-07-20 17:43:22 +0900

    백준 11660번 문제 - 구간 합 구하기5

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone11660.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone11660
+    {
+        public static void Problem()
+        {
+            var sr = new StreamReader(Console.OpenStandardInput());
+            var sw = new StreamWriter(Console.OpenStandardOutput());
+            var NM = sr.ReadLine().Split();
+            var N = int.Parse(NM[0]);
+            var M = int.Parse(NM[1]);
+            var arr = new int[2, N, N];
+            for (int i = 0; i < N; ++i)
+            {
+                var numbers = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+                for (int j = 0; j < N; ++j)
+                {
+                    arr[0, i, j] = j == 0 ? numbers[j] : arr[0, i, j - 1] + numbers[j];
+                    arr[1, i, j] = i == 0 ? arr[0, i, j] : arr[1, i - 1, j] + arr[0, i, j];
+                }
+            }
+            for (int i = 0; i < M; ++i)
+            {
+                var numbers = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+                var yL = numbers[0] - 1;
+                var xL = numbers[1] - 1;
+                var yR = numbers[2] - 1;
+                var xR = numbers[3] - 1;
+                var result = arr[1, yR, xR];
+                if (xL != 0)
+                    result -= arr[1, yR, xL - 1];
+    
+                if (yL != 0)
+                    result -= arr[1, yL - 1, xR];
+                if (xL != 0 && yL != 0)
+                    result += arr[1, yL - 1, xL - 1];
+                sw.WriteLine(result);
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -10,10 +10,6 @@ namespace ChallengeBaekjone
             var sr = new StreamReader(Console.OpenStandardInput());
             var sw = new StreamWriter(Console.OpenStandardOutput());
 
-            var arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
-            var x = arr[0] < arr[2] - arr[0] ? arr[0] : arr[2] - arr[0];  
-            var y = arr[1] < arr[3] - arr[1] ? arr[1] : arr[3] - arr[1];
-            sw.WriteLine(x < y ? x : y);
             
             sr.Close();
             sw.Close();
