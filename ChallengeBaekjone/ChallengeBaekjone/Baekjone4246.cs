354d6e832f8a055340ef4b76ddf9240c27b398fd
PoPo <dltngus7535@gmail.com>
2024-07-23 08:05:53 +0900

    백준  4246번 문제 - To and Fro

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone4246.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone4246
+    {
+        static void Problem()
+        {
+            var sr = new StreamReader(Console.OpenStandardInput());
+            var sw = new StreamWriter(Console.OpenStandardOutput());
+
+
+            while (true)
+            {
+                var size = int.Parse(sr.ReadLine());
+                
+                if(size == 0)
+                    break;
+
+                var str = sr.ReadLine();
+                var arr = new char[str.Length / size, size];
+                for (int i = 0; i < str.Length; ++i)
+                {
+                    var a = i / size;
+                    var b = i % size;
+                    arr[i == 0 ? 0 : a, 
+                        a % 2 == 0 ? b : size - b - 1] = str[i];
+                }
+
+                for (int x = 0; x < size; ++x)
+                    for (int y = 0; y < str.Length / size; ++y) 
+                        sw.Write(arr[y,x]);
+                sw.WriteLine();
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace ChallengeBaekjone
 {
@@ -10,54 +9,35 @@ namespace ChallengeBaekjone
         {
             var sr = new StreamReader(Console.OpenStandardInput());
             var sw = new StreamWriter(Console.OpenStandardOutput());
-
-            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int n = input[0], m = input[1], k = input[2];
-            var board = new char[n][];
-        
-            for (int i = 0; i < n; i++)
-                board[i] = Console.ReadLine().ToCharArray();
-
-            var wb = new int[n + 1, m + 1];
-            var bw = new int[n + 1, m + 1];
-
-            for (var i = 1; i <= n; i++)
-            {
-                for (var j = 1; j <= m; j++)
-                {
-                    wb[i, j] = wb[i - 1, j] + wb[i, j - 1] - wb[i - 1, j - 1];
-                    bw[i, j] = bw[i - 1, j] + bw[i, j - 1] - bw[i - 1, j - 1];
-                
-                    if ((i + j) % 2 == 0)
-                    {
-                        if (board[i - 1][j - 1] == 'B')
-                            ++wb[i, j];
-                        else
-                            ++bw[i, j];
-                    }
-                    else
-                    {
-                        if (board[i - 1][j - 1] == 'B')
-                            ++bw[i, j];
-                        else
-                            ++wb[i, j];
-                    }
-                }
-            }
-
-            var minPaint = int.MaxValue;
-
-            for (int i = k; i <= n; i++)
-            {
-                for (int j = k; j <= m; j++)
-                {
-                    var wbCount = wb[i, j] - wb[i - k, j] - wb[i, j - k] + wb[i - k, j - k];
-                    var bwCount = bw[i, j] - bw[i - k, j] - bw[i, j - k] + bw[i - k, j - k];
-                
-                    minPaint = Math.Min(minPaint, Math.Min(wbCount, bwCount));
-                }
-            }
-            Console.WriteLine(minPaint);
+            
+            // var N = int.Parse(sr.ReadLine());
+            // var arr = new int[N, N];
+            // var size = N;
+            //
+            // var check = false;
+            // while (check == false)
+            // {
+            //     for (int yPos = 0; yPos < N; yPos += size)
+            //     {
+            //         for (int xPos = 0; xPos < N; xPos += size)
+            //         {
+            //             var isWhite = arr[yPos, xPos] == 0;
+            //             var isCut = false;
+            //             for (int y = yPos; y < yPos + size; ++y) 
+            //             {
+            //                 for (int x = xPos; x < xPos + size; ++x) 
+            //                 {
+            //                     
+            //                 }
+            //             }
+            //         }
+            //     }
+            //
+            //     if (size == 1)
+            //         break;
+            //     size /= 2;
+            // }
+            //
             sr.Close();
             sw.Close();
         }
