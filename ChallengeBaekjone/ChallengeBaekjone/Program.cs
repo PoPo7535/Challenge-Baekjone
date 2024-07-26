be4ff9fd4b58c335c7bf1d3845d11950ce3b5976
PoPo <dltngus7535@gmail.com>
2024-07-26 22:54:44 +0900

    백준 1149번 문제 - RGB거리

--- ChallengeBaekjone/ChallengeBaekjone/Program.cs
+++ ChallengeBaekjone/ChallengeBaekjone/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Text;
+using System.Linq;
 
 namespace ChallengeBaekjone
 {
@@ -8,57 +7,7 @@ namespace ChallengeBaekjone
     {
         static void Main()
         {
-            var sr = new StreamReader(Console.OpenStandardInput());
-            var sw = new StreamWriter(Console.OpenStandardOutput());
 
-            var str= Console.ReadLine().Split();
-            var N = int.Parse(str[0]);
-            var M = int.Parse(str[1]);
-            var max = int.MinValue;
-            StringBuilder sb ;
-            for (int i = 1; i <= M; ++i)
-            {
-                var val = 0;
-                sb = new StringBuilder((N * i).ToString());
-                for (int j = 0; j < sb.Length; ++j)
-                    val += (int)Math.Pow(10, sb.Length - j) * (sb[sb.Length -j- 1] - '0');
-                val /= 10;
-                if (max < val)
-                    max = val;
-            }
-            Console.WriteLine(max);
-            
-            
-            // var N = int.Parse(sr.ReadLine());
-            // var arr = new int[N, N];
-            // var size = N;
-            //
-            // var check = false;
-            // while (check == false)
-            // {
-            //     for (int yPos = 0; yPos < N; yPos += size)
-            //     {
-            //         for (int xPos = 0; xPos < N; xPos += size)
-            //         {
-            //             var isWhite = arr[yPos, xPos] == 0;
-            //             var isCut = false;
-            //             for (int y = yPos; y < yPos + size; ++y) 
-            //             {
-            //                 for (int x = xPos; x < xPos + size; ++x) 
-            //                 {
-            //                     
-            //                 }
-            //             }
-            //         }
-            //     }
-            //
-            //     if (size == 1)
-            //         break;
-            //     size /= 2;
-            // }
-            //
-            sr.Close();
-            sw.Close();
         }
     }
 }
