5956fd0130507a05501a33df35984159ccfce6c1
PoPo7535 <143324551+PoPo7535@users.noreply.github.com>
2026-01-10 21:24:32 +0900

    백준 13305문제 - 주유소 백준 1654문제 - 랜선 자르기

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone13305.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone13305
+    {
+        void Problem()
+        {
+            var sr = new StreamReader(Console.OpenStandardInput());
+            var sw = new StreamWriter(Console.OpenStandardOutput());
+            var t = int.Parse(sr.ReadLine());
+            var dis = new long[t - 1];
+            var str = sr.ReadLine().Split(' ');
+            for (int i = 0; i < str.Length; i++)
+                dis[i] = long.Parse(str[i]);
+            str = sr.ReadLine().Split(' ');
+            var gasValue = long.Parse(str[0]);
+            var result = gasValue * dis[0];
+            for (int i = 1; i < str.Length - 1; i++)
+            {
+                var newGasValue = long.Parse(str[i]);
+                if (newGasValue < gasValue)
+                    gasValue = newGasValue;
+                result += gasValue * dis[i];
+            }
+            sw.WriteLine(result);
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone1654.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone1654
+    {
+        void Problem()
+        {
+            var input = Console.ReadLine().Split(' ');
+            int K = int.Parse(input[0]);
+            int N = int.Parse(input[1]);
+            int[] arr = new int[K];
+            long max = 0;
+            for (int i = 0; i < K; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            long left = 1;
+            long right = max;
+            long result = 0;
+            while (left <= right)
+            {
+                long mid = (left + right) / 2;
+                long count = 0;
+                foreach (var len in arr)
+                    count += len / mid;
+                if (count >= N)
+                {
+                    result = mid;   
+                    left = mid + 1; 
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
