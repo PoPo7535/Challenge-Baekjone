609b1dce5fa6939b96802b265ea5eb08b0ada4c3
PoPo7535 <143324551+PoPo7535@users.noreply.github.com>
2026-01-15 23:11:47 +0900

    백준 32334문제 - 자석체스 백준 32930문제 - 슈팅연습

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone32930.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone32930
+    {
+        void Problem()
+        {
+            var sr = new StreamReader(Console.OpenStandardInput());
+            var sw = new StreamWriter(Console.OpenStandardOutput());
+            var input = sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int N = input[0];
+            int M = input[1];
+            var list = new LinkedList<(int y, int x)>();
+            for (int i = 0; i < N + M; i++)
+            {
+                input = sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                list.AddLast((input[0], input[1]));
+            }
+            var result = 0;
+            (int y, int x) pos = (0, 0);
+            for (int m = 0; m < M; m++)
+            {
+                var node = list.First;
+                var removeNode = list.First;
+                var max = 0;
+                for (int n = 0; n < N; n++)
+                {
+                    var xVal = Math.Abs(pos.x - node.Value.x);
+                    xVal *= xVal;
+                    var yVal = Math.Abs(pos.y - node.Value.y);
+                    yVal *= yVal;
+                    if (max < xVal + yVal)
+                    {
+                        max = xVal + yVal;
+                        removeNode = node;
+                    }
+                    node = node.Next;
+                }
+                pos = removeNode.Value;
+                list.Remove(removeNode);
+                result += max;
+            }
+            sw.WriteLine(result);
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjoon32334.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjoon32334
+    {
+        void Problem()
+        {
+            var sr = new StreamReader(Console.OpenStandardInput());
+            var sw = new StreamWriter(Console.OpenStandardOutput());
+            var input = sr.ReadLine().Split(' ');
+            var N = int.Parse(input[0]);
+            var D = int.Parse(input[1]);
+            var arr = new int[N, N];
+            for (int y = 0; y < N; y++)
+            {
+                var numbers = sr.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int x = 0; x < N; x++)
+                {
+                    if (numbers[x] != 1)
+                        continue;
+                    arr[y, x] = 2;
+                    for (int areaY = -D; areaY < D + 1; areaY++)
+                    {
+                        for (int areaX = -D; areaX < D + 1; areaX++)
+                        {
+                            var offX = areaX + x;
+                            var offY = areaY + y;
+                            if (offY < 0 || offY >= N ||
+                                offX < 0 || offX >= N)
+                                continue;
+                            ++arr[offY, offX];
+                        }
+                    }
+                }
+            }
+            (int y, int x) pos = (0, 0);
+            for (int y = 0; y < N; y++)
+            {
+                for (int x = 0; x < N; x++)
+                {
+                    if (arr[y, x] < arr[pos.y, pos.x])
+                        pos = (y, x);
+                }
+            }
+            sw.WriteLine($"{pos.y + 1} {pos.x + 1}");
+            if (1 <= arr[pos.y, pos.x])
+                sw.WriteLine(arr[pos.y, pos.x]);
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
