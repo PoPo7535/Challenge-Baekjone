d7bc84366e7ed6be1fcceb956df4233d58948d50
PoPo7535 <143324551+PoPo7535@users.noreply.github.com>
2026-01-16 22:27:32 +0900

    백준 16395문제 - 파스칼의 삼각형

---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjoon16395.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjoon16395
+    {
+        void Problem()
+        {
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var n = arr[0] - 1;
+            var k = arr[1] - 1; 
+            Console.WriteLine(Cal(n, k));
+
+            static int Cal(int n, int k)
+            {
+                int result = 1;
+                for (int i = 1; i <= k; i++)
+                    result = result * (n - i + 1) / i;
+                return result;
+            }
+        }
+    }
+}
