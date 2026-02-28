4c5e8b000f9749e9883a384c44bfd5e1ae2156da
PoPo7535 <143324551+PoPo7535@users.noreply.github.com>
2026-03-01 04:46:16 +0900

    백준 14725문제 - 개미굴

---
+++ ChallengeBaekjone/ChallengeBaekjone/BaekJoon1120.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ChallengeBaekjone
+{
+    public class BaekJoon1120
+    {
+        void Problem()
+        {
+            var split = Console.ReadLine().Split(' ');
+            var count = Math.Abs(split[0].Length - split[1].Length);
+            var bigStr = split[0].Length < split[1].Length ? split[1] : split[0];
+            var smallStr = split[0].Length < split[1].Length ? split[0] : split[1];
+            var arr = new int[count + 1];
+            for (int i = 0; i < smallStr.Length; i++)
+            {
+                for (int j = 0; j <= count; j++)
+                {
+                    if (bigStr[i + j] != smallStr[i])
+                    {
+                        ++arr[j];
+                    }
+                }
+            }
+            Console.WriteLine(arr.Min());
+        }
+    }
+}
---
+++ ChallengeBaekjone/ChallengeBaekjone/Baekjone14725.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ChallengeBaekjone
+{
+    public class Baekjone14725
+    {
+        static void Problem()
+        {
+            var sr = new StreamReader(Console.OpenStandardInput());
+            var sw = new StreamWriter(Console.OpenStandardOutput());
+
+            var N = int.Parse(sr.ReadLine());
+            var root = new Node("");
+
+            for (int i = 0; i < N; i++)
+            {
+                var input = sr.ReadLine().Split(' ');
+                root.Add(input.Skip(1).ToArray());
+            }
+
+            root.Write(sw);
+
+            sw.Flush();
+            sw.Close();
+            sr.Close();
+        }
+
+        public class Node
+        {
+            private string name;
+            private Dictionary<string, Node> children;
+
+            public Node(string name)
+            {
+                this.name = name;
+                children = new Dictionary<string, Node>();
+            }
+
+            public void Add(string[] foods)
+            {
+                Node current = this;
+
+                foreach (var food in foods)
+                {
+                    if (!current.children.ContainsKey(food))
+                    {
+                        current.children[food] = new Node(food);
+                    }
+
+                    current = current.children[food];
+                }
+            }
+
+            public void Write(StreamWriter sw, int depth = 0)
+            {
+                var keys = children.Keys.ToList();
+                keys.Sort();
+
+                foreach (var key in keys)
+                {
+                    for (int i = 0; i < depth; i++)
+                    {
+                        sw.Write("--");
+                    }
+
+                    sw.WriteLine(key);
+                    children[key].Write(sw, depth + 1);
+                }
+            }
+        }
+    }
+}
